5beedcc047539c7ee79fa9185da074b8b60acd5b
harutoreon <norabder567@gmail.com>
2023-07-02 10:48:15 +0900

    Update reverse effect

--- test/effects_test.rb
+++ test/effects_test.rb
@@ -1,8 +1,9 @@
 require 'minitest/autorun'
-require './lib/effects.rb'
+require './lib/effects'
 
 class EffectsTest < Minitest::Test
   def test_reverse
-    assert Effects
+    effect = Effects.reverse
+    assert_equal 'ybuR si !nuf', effect.call('Ruby is fun!')
   end
 end
