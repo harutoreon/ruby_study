dab4ef86c425ddac0b04df5d8d2b261008995777
harutoreon <norabder567@gmail.com>
2024-09-07 19:52:21 +0900

    Add 022.rb

---
+++ samples/chapter-02/022.rb
@@ -0,0 +1,13 @@
+binary = 0b1011111011101111
+p binary  #=> 48879
+
+octal = 0o137357
+p octal  #=> 48879
+p octal == 0137357  #=> true
+
+decimal = 0d48879
+p decimal  #=> 48879
+
+hexadecimal = 0xBEEF
+p hexadecimal  #=> 48879
+p hexadecimal == 0xbeef  #=> true
