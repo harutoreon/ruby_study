844c18caa0387fa3e44edad2644c16b5a414bcb6
harutoreon <norabder567@gmail.com>
2024-09-16 05:27:30 +0900

    Add 051.rb

---
+++ samples/chapter-03/051.rb
@@ -0,0 +1,13 @@
+divisor = 10
+
+unless divisor == 0
+  puts "100を#{divisor}で割った数は#{100 / divisor}です"
+end
+
+divisor = 0
+
+unless divisor == 0
+  puts "100を#{divisor}で割った数は#{100 / divisor}です"
+elsif
+  puts "0で割ることはできません"
+end
