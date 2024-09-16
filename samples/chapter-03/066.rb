0cd108e9c7dead5f02e214a5bc2cf6ad1c99d171
harutoreon <norabder567@gmail.com>
2024-09-17 04:16:14 +0900

    Modify 066.rb

--- samples/chapter-03/066.rb
+++ samples/chapter-03/066.rb
@@ -10,43 +10,43 @@
 #   puts 'ブロック終了'
 # end
 
-class BlockCaller
-  def initialize(&block)
-    @block = block
-  end
+# class BlockCaller
+#   def initialize(&block)
+#     @block = block
+#   end
 
-  def execute
-    @block.call
-  end
-end
+#   def execute
+#     @block.call
+#   end
+# end
 
-def execute_block_caller
-  puts 'メソッド開始'
+# def execute_block_caller
+#   puts 'メソッド開始'
 
-  block_caller = BlockCaller.new do
-    puts 'ブロック開始'
-    return
-    puts 'ブロック終了'
-  end
+#   block_caller = BlockCaller.new do
+#     puts 'ブロック開始'
+#     return
+#     puts 'ブロック終了'
+#   end
 
-  block_caller.execute
+#   block_caller.execute
 
-  puts 'メソッド終了'
-end
+#   puts 'メソッド終了'
+# end
 
-execute_block_caller
+# execute_block_caller
 
-def create_block_caller
-  BlockCaller.new do
-    puts 'ブロック開始'
-    return
-    puts 'ブロック終了'
-  end
-end
+# def create_block_caller
+#   BlockCaller.new do
+#     puts 'ブロック開始'
+#     return
+#     puts 'ブロック終了'
+#   end
+# end
 
-begin
-  block_caller = create_block_caller
-  block_caller.execute
-rescue => e
-  p e
-end
+# begin
+#   block_caller = create_block_caller
+#   block_caller.execute
+# rescue => e
+#   p e
+# end
