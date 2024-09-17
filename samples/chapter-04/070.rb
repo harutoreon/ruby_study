0267ae447ba0b563280a062b118e352144bdc5d7
harutoreon <norabder567@gmail.com>
2024-09-18 01:38:12 +0900

    Add 070.rb

---
+++ samples/chapter-04/070.rb
@@ -0,0 +1,20 @@
+array = [1, 2, 3, 4, 5, 4, 3]
+array.delete(3)
+
+p array
+
+deleted_value = array.delete_at(1)
+
+p deleted_value
+p array
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+deleted_value = array.pop
+
+p deleted_value
+p array
+
+delete_value = array.shift(3)
+
+p delete_value
+p array
