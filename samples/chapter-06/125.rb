dcb8bb50e7aba8bd76e8d5a3cff126af2bd8e261
harutoreon <norabder567@gmail.com>
2024-09-29 08:19:44 +0900

    Modify 125.rb

--- samples/chapter-06/125.rb
+++ samples/chapter-06/125.rb
@@ -5,3 +5,13 @@ p 'Lorem ipsum dolor sit amet'.scan(/.m/)
 #=> "EM"
 #   "UM"
 #   "AM"
+
+p 'Lorem ipsum dolor sit amet'.scan(/.(.)(m)/)
+#=> [["e", "m"], ["u", "m"], ["a", "m"]]
+
+'Lorem ipsum dolor sit amet'.scan(/.(.)(m)/) do |s1, s2|
+  puts "matched: #{s1} #{s2}"
+end
+#=> matched: e m
+#   matched: u m
+#   matched: a m
