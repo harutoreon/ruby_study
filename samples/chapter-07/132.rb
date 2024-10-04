394dc37c40e22d4fa65423c8a6e08f8b000166a0
harutoreon <norabder567@gmail.com>
2024-10-05 04:22:54 +0900

    Modify 132.rb

--- samples/chapter-07/132.rb
+++ samples/chapter-07/132.rb
@@ -13,3 +13,14 @@ payment2 = Payment.new(amount: 1000)
 
 p payment1.total_amount  #=> 1000
 p payment2.total_amount  #=> 1500
+
+class Calc
+  def heavy_operation
+    @result ||= sleep(3)
+  end
+end
+
+calc = Calc.new
+
+p calc.heavy_operation  #=> 3, この結果が表示されるまでには3秒かかる
+p calc.heavy_operation  #=> 3, この表示はだたちに表示される
