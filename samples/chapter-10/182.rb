1ee94c54daa9f197a2274cd650c4609bbfb1c95e
harutoreon <norabder567@gmail.com>
2024-10-24 19:47:55 +0900

    Add 182.rb

---
+++ samples/chapter-10/182.rb
@@ -0,0 +1,3 @@
+puts "exp(0) = #{Math.exp(0)}"              #=> exp(0) = 1.0
+puts "exp(1) = #{Math.exp(1)}"              #=> exp(1) = 2.718281828459045
+puts "exp(√2) = #{Math.exp(Math.sqrt(2))}"  #=> exp(√2) = 4.1132503787829275
