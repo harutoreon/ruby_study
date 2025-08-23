b5b49939578c07c8c13bd02f05e9315a08a3032a
reonharuto <norabder567@gmail.com>
2025-08-23 14:22:16 +0900

    Add 262.rb and test.db

--- samples/chapter-19/262.rb
+++ samples/chapter-19/262.rb
@@ -1,1 +1,9 @@
-puts 'hello world.'
+require 'sqlite3'
+
+databese = SQLite3::Database.new('test.db')
+
+SQLite3::Database.new('test.db') do |db|
+
+end
+
+databese = SQLite3::Database.new(':memory:')
