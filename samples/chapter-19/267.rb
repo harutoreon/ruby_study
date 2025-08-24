be8652aeff7385c4554f50707e513c4c237fd8d3
reonharuto <norabder567@gmail.com>
2025-08-24 19:34:09 +0900

    Add update and delete statement to 267.rb

--- samples/chapter-19/267.rb
+++ samples/chapter-19/267.rb
@@ -15,3 +15,25 @@ statement = client.prepare("SELECT * FROM users WHERE id IN (?, ?)")
 statement.execute(4, 6).each do |row|
   p row
 end
+
+client = Mysql2::Client.new(host: 'localhost', username: 'root', password: 'password', database: 'test')
+client.query('SELECT * FROM users').each do |row|
+  p row
+end
+
+client.query("UPDATE users SET email = 'Clarice@example.com' WHERE id = 6")
+
+client.query('SELECT * FROM users').each do |row|
+  p row
+end
+
+client = Mysql2::Client.new(host: 'localhost', username: 'root', password: 'password', database: 'test')
+client.query('SELECT * FROM users').each do |row|
+  p row
+end
+
+client.query('DELETE FROM users WHERE id = 6')
+
+client.query('SELECT * FROM users').each do |row|
+  p row
+end
