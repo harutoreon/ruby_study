7f5a6e1c3ee381f5b3ac9c6a41180e7c4c38c3a3
reonharuto <norabder567@gmail.com>
2025-08-24 17:17:32 +0900

    Add 267.rb

---
+++ samples/chapter-19/267.rb
@@ -0,0 +1,17 @@
+require 'mysql2'
+
+client = Mysql2::Client.new(host: 'localhost', username: 'root', password: 'password', database: 'test')
+client.query('SELECT * FROM users').each do |row|
+  p row
+end
+
+client = Mysql2::Client.new(host: 'localhost', username: 'root', password: 'password', database: 'test')
+client.query('SELECT * FROM users').each(as: :array) do |row|
+  p row
+end
+
+client = Mysql2::Client.new(host: 'localhost', username: 'root', password: 'password', database: 'test')
+statement = client.prepare("SELECT * FROM users WHERE id IN (?, ?)")
+statement.execute(4, 6).each do |row|
+  p row
+end
