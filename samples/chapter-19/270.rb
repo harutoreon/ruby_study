014bb1a805d38ca9dcaef6ea6e52e5a5fc15897c
reonharuto <norabder567@gmail.com>
2025-08-26 04:01:09 +0900

    Add update and delete statement to 270.rb

--- samples/chapter-19/270.rb
+++ samples/chapter-19/270.rb
@@ -2,14 +2,24 @@ require 'pg'
 
 conn = PG.connect(host: 'localhost', user: 'postgres', password: 'password', dbname: 'test')
 
-# conn.exec('SELECT * FROM users').each do |row|
-#   p row
-# end
+conn.exec('SELECT * FROM users').each do |row|
+  p row
+end
 
-# conn.exec('SELECT * FROM users').values.each do |row|
-#   p row
-# end
+conn.exec('SELECT * FROM users').values.each do |row|
+  p row
+end
 
 conn.exec_params("SELECT * FROM users WHERE id IN ($1, $2)", [1, 3]).each do |row|
+  p row
+end
+
+conn.exec("UPDATE users SET email = 'Alice@example.com' WHERE id = 1")
+conn.exec('SELECT * FROM users').each do |row|
+  p row
+end
+
+conn.exec("DELETE FROM users WHERE id = 1")
+conn.exec('SELECT * FROM users').each do |row|
   p row
 end
