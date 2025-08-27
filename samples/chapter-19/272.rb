3205ce472cf79195557fecd957b99f23ff97334a
reonharuto <norabder567@gmail.com>
2025-08-28 01:53:40 +0900

    Add list to 272.rb

--- samples/chapter-19/272.rb
+++ samples/chapter-19/272.rb
@@ -2,5 +2,12 @@ require 'redis'
 
 redis = Redis.new(host: 'localhost')
 redis.set('hello', 'word')
+p
+ redis.get('hello')
 
-p redis.get('hello')
+redis = Redis.new(host: 'localhost')
+redis.rpush('queue', 'foo')
+redis.rpush('queue', 'bar')
+
+p redis.lpop('queue')
+p redis.lpop('queue')
